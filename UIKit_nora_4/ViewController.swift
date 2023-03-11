a1957378be0168f7bcee2332a1a80c11f20a549f
lglmva <62218130+lglmva@users.noreply.github.com>
2023-03-11 17:36:45 +0300

    Initial Commit

---
+++ UIKit_nora_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_nora_4
+//
+//  Created by Oleg Lomov on 11/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
